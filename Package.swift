c83a6ce09982c371789d35b7686d3482f1ef2a69
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2024-03-27 12:50:09 +0900

    changed the location privacy-info

--- Package.swift
+++ Package.swift
@@ -24,7 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Mugen",
-            path: "Sources",
             resources: [.copy("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
